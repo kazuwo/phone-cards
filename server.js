db35508c1ade68b40b1ce4aefbdca94bb6fc04de
kazuwo <623taiki@gmail.com>
2017-01-18 23:27:57 +0900

    fix type

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ server.on('request', function(req, res){
   res.write('hello world !!!');
   res.end();
 });
-server..listen(process.env.PORT || 5000)
+server.listen(process.env.PORT || 5000)
 console.log("server listening ...");
 
