0db5beb8099a08a70fe4b91b7151f24c7fd032ce
kazuwo <623taiki@gmail.com>
2017-01-20 10:21:45 +0900

    failed use bootstrap
    i can use bootstrap in local
    but when using node.js's server ,i failed

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var server = http.createServer();
 server.on('request', function(req, res){
-  fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data){
+  fs.readFile(__dirname + '/public_html/index.html', 'utf-8', function(err, data){
     if(err){
       res.writeHead(404, {'Content-Type': 'text/plain'});
       res.write('not found !');
@@ -14,7 +14,8 @@ server.on('request', function(req, res){
   });
 
 });
-server.listen(process.env.PORT || 5000)
+//server.listen(process.env.PORT || 5000)
+server.listen(1337, '172.16.8.215');
 console.log("server listening ...");
 
 
